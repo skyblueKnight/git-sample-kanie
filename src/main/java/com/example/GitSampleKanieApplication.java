f370585b8ab353416235dccf23634954367a81de
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-05-27 12:05:53 +0900

    改行もできる。

---
+++ src/main/java/com/example/GitSampleKanieApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKanieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKanieApplication.class, args);
+	}
+
+}
